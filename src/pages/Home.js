c371c65adbbdbe0ec55725738c04406300692a46
Dandun Adi <dandun.adi@gmail.com>
2021-05-20 06:10:54 +0700

    add home layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,45 @@
+import styles from "./Home.module.css";
+
 const Home = () => {
   return (
-    <div className="container">
-      <h1>Home</h1>
-    </div>
+    <main className="container">
+      <section className="grid">
+        <div className={styles.title}>
+          <p className={styles.logo}>WARUNG BUNGSU</p>
+          <h1 className={styles.lead + " xl"}>
+            Dari Bahan Segar Dengan Kualitas Terbaik
+          </h1>
+        </div>
+      </section>
+      <div className={styles.cream}></div>
+      <div className={styles.featured}>
+        <img className={styles.ayam} src="/IMG.png" alt="ayams" />
+        <div className={styles.featuredOrange}></div>
+        <div className={styles.featuredGreen}></div>
+      </div>
+      <div className={styles.open}>
+        <span className={styles.openBtn}>
+          <i class="far fa-clock"></i> BUKA SETIAP HARI
+        </span>
+      </div>
+      <div className={styles.order + " sm"}>
+        <i class="fa fa-utensils text-orange"></i> <span>TERIMA PESANAN</span>
+      </div>
+      <div className={styles.attribution}>
+        <p>
+          Coded by{" "}
+          <a
+            href="https://www.dandun.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.attributionLink}
+          >
+            DDD
+          </a>{" "}
+        </p>
+        <p>All Right Reserved - Warung Bungsu &copy;2021</p>
+      </div>
+    </main>
   );
 };
 
