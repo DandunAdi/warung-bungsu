a680a85c0a6adcf1e5288fd88b3cae20346a6a92
Dandun Adi <dandun.adi@gmail.com>
2021-05-21 20:39:32 +0700

    add menu card

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -5,18 +5,18 @@ const Navbar = () => {
     <header className={styles.header + " flex text-center"}>
       <div className={styles.container}>
         <div className={styles.navLink + " btn btn-primary flex"}>
-          <i className="fa fa-home lg"></i>
+          <i className="fa fa-home"></i>
         </div>
 
         <div className={styles.navLink + " btn btn-primary flex"}>
-          <i className="fa fa-utensils lg"></i>
+          <i className="fa fa-utensils "></i>
         </div>
 
         <div className={styles.navLink + " btn btn-primary flex"}>
-          <i className="fa fa-phone-alt lg"></i>
+          <i className="fa fa-phone-alt "></i>
         </div>
 
-        <i className="fab fa-instagram lg"></i>
+        <i className="fab fa-instagram "></i>
       </div>
     </header>
   );
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,8 +1,62 @@
+import styles from "./Menu.module.css";
+
+const meals = [
+  {
+    id: 1,
+    name: "Ayam Goreng",
+    image: "/ayam.png",
+  },
+  {
+    id: 2,
+    name: "Ayam Bakar",
+    image: "/ayam.png",
+  },
+  {
+    id: 3,
+    name: "Ayam Bakar Madu",
+    image: "/ayam.png",
+  },
+  {
+    id: 4,
+    name: "Bebek Goreng",
+    image: "/bebek.png",
+  },
+  {
+    id: 5,
+    name: "Bebek Bakar",
+    image: "/bebek.png",
+  },
+  {
+    id: 6,
+    name: "Bebek Bakar Madu",
+    image: "/bebek.png",
+  },
+];
+
 const Menu = () => {
   return (
-    <div>
-      <h1>Menu</h1>
-    </div>
+    <section className={styles.menu}>
+      <h2 className={styles.title + " lg text-center"}>
+        Aneka Sajian Dengan Rasa Mantap Harga Hemat
+      </h2>
+
+      <div className={styles.content}>
+        {meals.map((meal) => (
+          <div className={styles.card} key={meal.id}>
+            <img src={meal.image} alt={meal.name} />
+            <h3 className={styles.cardTitle + " lead text-center"}>
+              {meal.name}
+            </h3>
+          </div>
+        ))}
+        <div className={styles.card}>
+          <img src="/ayam.png" alt="ayam-goreng" />
+          <h3 className={styles.cardTitle + " lead text-center"}>
+            Ayam Goreng
+          </h3>
+        </div>
+      </div>
+    </section>
   );
 };
 
