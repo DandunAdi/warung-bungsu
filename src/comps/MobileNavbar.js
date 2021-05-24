0b8217a635d359306a900ddb78525b0d9b4f9b49
Dandun Adi <dandun.adi@gmail.com>
2021-05-24 22:41:07 +0700

    adjust nav

--- src/comps/MobileNavbar.js
+++ src/comps/MobileNavbar.js
@@ -86,7 +86,7 @@ const container = {
 };
 
 const item = {
-  hidden: { x: "100vh", opacity: 0 },
+  hidden: { x: "100vw", opacity: 0 },
   show: { x: 0, opacity: 1, transition: { duration: 0.5, type: "spring" } },
   exit: { x: "100vw", transition: { duration: 0.5 } },
 };
